906ae79fc0734fe00c94d2bd52216f3ea77f89ed
Pedro Cavalcante <pedrocavalcantea02@gmail.com>
2023-03-29 13:35:29 -0300

    gerando as figurinhas

--- aluraStickers/src/aluraStickers/App.java
+++ aluraStickers/src/aluraStickers/App.java
@@ -1,6 +1,8 @@
 package aluraStickers;
 
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -28,12 +30,24 @@ public class App {
 		Collection<Map<String, String>> listaDeFilmes = parser.parse(body);
 		// System.out.println(listaDeFilmes.size());
 
-		for (Map<String, String> filme : listaDeFilmes) {
-			System.out.println(filme.get("title"));
+		// exibir e manipular os dados 
+        var geradora = new GeradoraDeFigurinhas();
+        for (Map<String,String> filme : listaDeFilmes) {
+
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
+
+            geradora.cria(inputStream, nomeArquivo);
+            System.out.println(filme.get("title"));
 			System.out.println(filme.get("image"));
 			System.out.println(filme.get("imDbRating"));
-			System.out.println("é");
-		}
+			System.out.println("");
+            System.out.println(titulo);
+            System.out.println();
+        }
 
 	}
 }
